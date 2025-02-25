627426e4eafb5ab0f8b29e06d5d1e7c29db43f41
Leo Cazenille <leo.cazenille@gmail.com>
2025-02-25 13:22:38 +0100

    update build.sh script to work with macos

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# Set default build type if not provided
 config=${1:-"Release"}
+
+# Detect OS
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+    NUM_CORES=$(sysctl -n hw.ncpu)  # macOS
+else
+    NUM_CORES=$(nproc)  # Linux
+fi
 
 # Check if sudo is available
 if command -v sudo &> /dev/null; then
@@ -8,6 +18,9 @@ else
     SUDO=""
 fi
 
+# Configure CMake
 cmake -S . -B build -DCMAKE_BUILD_TYPE=$config
-cmake --build build -- -j$(nproc) && $SUDO cmake --build build --target install && cmake --build build --target examples -- -j$(nproc)
+
+# Build project
+cmake --build build -- -j$NUM_CORES && $SUDO cmake --install build && cmake --build build --target examples -- -j$NUM_CORES
 
