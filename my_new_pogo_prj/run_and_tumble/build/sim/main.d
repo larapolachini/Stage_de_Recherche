400564c8bdf99901f181b8acd84ea45525bc8cba
Lara Polachini <larapolachini@uol.com.br>
2025-05-12 21:44:33 +0200

    confi

---
+++ my_new_pogo_prj/run_and_tumble/build/sim/main.d
@@ -0,0 +1,8 @@
+build/sim/main.o: main.c ../../src/pogobase.h ../../src/pogosim/version.h \
+ ../../src/pogosim/pogosim.h ../../src/pogosim/spogobot.h \
+ ../../src/pogosim/FifoBuffer.h
+../../src/pogobase.h:
+../../src/pogosim/version.h:
+../../src/pogosim/pogosim.h:
+../../src/pogosim/spogobot.h:
+../../src/pogosim/FifoBuffer.h:
