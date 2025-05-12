f6e6552eed471633485fade3f3700d5192623210
Lara Polachini <larapolachini@uol.com.br>
2025-05-12 11:41:36 +0200

    organizing

---
+++ my_new_pogo_prj/build/sim/main.d
@@ -0,0 +1,1 @@
+build/sim/main.o: main.c
---
+++ my_new_pogo_prj/main.c
@@ -0,0 +1,82 @@
+
+// Main include for pogobots, both for real robots and for simulations
+#include "pogobase.h"
+
+// "Global" variables should be inserted within the USERDATA struct.
+// /!\  In simulation, don't declare non-const global variables outside this struct, elsewise they will be shared among all agents (and this is not realistic).
+typedef struct {
+    // Put all global variables you want here.
+    uint8_t data_foo[8];
+    time_reference_t timer_it;
+} USERDATA;
+
+// Call this macro in the same file (.h or .c) as the declaration of USERDATA
+DECLARE_USERDATA(USERDATA);
+
+// Don't forget to call this macro in the main .c file of your project (only once!)
+REGISTER_USERDATA(USERDATA);
+// Now, members of the USERDATA struct can be accessed through mydata->MEMBER. E.g. mydata->data_foo
+//  On real robots, the compiler will automatically optimize the code to access member variables as if they were true globals.
+
+
+// Init function. Called once at the beginning of the program (cf 'pogobot_start' call in main())
+void user_init(void) {
+#ifndef SIMULATOR
+    printf("setup ok\n");
+#endif
+
+    // Init timer
+    pogobot_stopwatch_reset(&mydata->timer_it);
+
+    // Set main loop frequency, message sending frequency, message processing frequency
+    main_loop_hz = 60;      // Call the 'user_step' function 60 times per second
+    max_nb_processed_msg_per_tick = 0;
+    // Specify functions to send/transmit messages. See the "hanabi" example to see message sending/processing in action!
+    msg_rx_fn = NULL;       // If Null, no reception of message
+    msg_tx_fn = NULL;       // If Null, don't send any message
+
+    // Set led index to show error codes (e.g. time overflows)
+    error_codes_led_idx = 3; // Default value, negative values to disable
+}
+
+
+// Step function. Called continuously at each step of the pogobot main loop
+void user_step(void) {
+    if (pogobot_ticks % 1000 == 0 && pogobot_helper_getid() == 0) {     // Only print messages for robot 0
+        printf(" HELLO WORLD !!!   Robot ID: %d   Current time: %lums  Timer: %luµs   pogobot_ticks: %lu\n",
+                pogobot_helper_getid(),
+                current_time_milliseconds(),
+                pogobot_stopwatch_get_elapsed_microseconds(&mydata->timer_it),
+                pogobot_ticks       // Increased by one at each execution of user_step
+                );
+    }
+
+    if ((uint32_t)(current_time_milliseconds() / 10000) % 2 == 0) {
+        pogobot_led_setColor(0,0,255);
+        pogobot_motor_set(motorL, motorFull);
+        pogobot_motor_set(motorR, motorStop);
+    } else {
+        pogobot_led_setColor(255,0,0);
+        pogobot_motor_set(motorL, motorStop);
+        pogobot_motor_set(motorR, motorFull);
+    }
+
+    mydata->data_foo[0] = 42;
+}
+
+
+// Entrypoint of the program
+int main(void) {
+    pogobot_init();     // Initialization routine for the robots
+#ifndef SIMULATOR
+    printf("init ok\n");
+#endif
+
+    // Specify the user_init and user_step functions
+    pogobot_start(user_init, user_step);
+    return 0;
+}
+
+// MODELINE "{{{1
+// vim:expandtab:softtabstop=4:shiftwidth=4:fileencoding=utf-8
+// vim:foldmethod=marker
