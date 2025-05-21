cea85ba6e21edf7635898adac5233cced1bb435c
Lara Polachini <larapolachini@uol.com.br>
2025-05-21 13:37:28 +0200

    distance

--- my_new_pogo_prj/distance.py
+++ my_new_pogo_prj/distance.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist, squareform
 
-#dist = pd.read_feather("results/result.feather")
-#print(dist)
+dist = pd.read_feather("results/result.feather")
+print(dist)
 
 
-def interindividual_distance_mean(feather_path: str) -> float:
+def interindividual_distance_mean(feather_path: str, communication_radius: float = 80.0) -> float:
     # Get the data
     df = pd.read_feather(feather_path)
 
@@ -22,17 +22,26 @@ def interindividual_distance_mean(feather_path: str) -> float:
         last_positions = run_df[run_df["time"] == last_time][["x", "y"]].to_numpy()
 
         # If we have at least 2 robots
-        if last_positions.shape[0] < 2:
+        if last_positions.shape[0] < 2:  #não é isso, preciso mudar para avaliar se tem um robo vizinho dentro do raio de communication_radius: 80.0 
             continue
 
         # Calculation of Euclidean distances between all pairs of agents
-        distances = pdist(last_positions, metric='euclidean')
-        mean_distance = distances.mean()
-        run_distances.append(mean_distance)
+        distance_matrix = pdist(last_positions, metric = 'euclidean')
+
+        # Filter juts the distances <= communication_radius
+        filtered_distances = distance_matrix[distance_matrix <= communication_radius]
+
+        if len(filtered_distances) == 0:
+            continue  # Aucun voisin trouvé
+
+        mean_filtered_distances = filtered_distances.mean()
+        run_distances.append(mean_filtered_distances)
         #print(last_positions)
 
     # Average distances over all runs
+    if len(run_distances) == 0:
+        return  float('nan')
     return np.mean(run_distances)
 
-score = interindividual_distance_mean("results/result.feather")
+score = interindividual_distance_mean("results/result.feather", communication_radius=80.0)
 print(f"Average interindividual distance : {score}")
