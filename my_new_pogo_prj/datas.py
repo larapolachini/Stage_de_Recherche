bba584250852f88656434e19a8aad5f1e29776c3
Lara Polachini <larapolachini@uol.com.br>
2025-05-23 15:24:23 +0200

    cma-es

--- my_new_pogo_prj/datas.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_feather("frames/data.feather")
-print(df)
--- my_new_pogo_prj/distance.py
+++ my_new_pogo_prj/distance.py
@@ -3,17 +3,22 @@ import numpy as np
 from scipy.spatial.distance import pdist
 
 dist = pd.read_feather("results/result.feather")
-print(dist)
+#print(dist)
 
 
-def interindividual_distance_mean(feather_path: str, communication_radius: float = 80.0) -> float:
+def interindividual_distance_mean(feather_path: str, communication_radius: float = 80.0) -> float:  # communication radius en mm et x, y en cm?
     # Get the data
     df = pd.read_feather(feather_path)
+    #print(f"{len(df)} lignes chargées depuis {feather_path}")
 
     # List to store average distance per run
     run_distances = []
 
     # Go through each run
+    if "run" not in df.columns:
+        #print("⚠️ Colonne 'run' absente, ajout d'un identifiant par défaut")
+        df["run"] = 0  # ou un identifiant plus utile si tu veux
+
     for run_id, run_df in df.groupby("run"):
         # Last timestamp of the run
         last_time = run_df["time"].max()
@@ -33,10 +38,14 @@ def interindividual_distance_mean(feather_path: str, communication_radius: float
                 mean_distance = np.mean(filtered_distances)
                 run_distances.append(mean_distance)
 
+            if len(filtered_distances) == 0:
+                print(f"Run {run_id} : aucune distance dans le rayon {communication_radius}")
+
+
     # Average for all runs
     if len(run_distances) == 0:
         return float('nan')
     return np.mean(run_distances)
 
-score = interindividual_distance_mean("results/result.feather", communication_radius=80.0)
+score = interindividual_distance_mean("results/result.feather", communication_radius = 80.0)
 print(f"Average interindividual distance : {score}")
