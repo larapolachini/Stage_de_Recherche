6f3881c8e2df377b964532e52201bda8660cd57f
Lara Polachini <larapolachini@uol.com.br>
2025-08-04 20:27:21 +0200

    ignoring

--- my_new_pogo_prj/examples/walls/build/sim/main.d
+++
@@ -1,8 +0,0 @@
-build/sim/main.o: main.c ../../src/pogobase.h ../../src/pogosim/version.h \
- ../../src/pogosim/pogosim.h ../../src/pogosim/spogobot.h \
- ../../src/pogosim/FifoBuffer.h
-../../src/pogobase.h:
-../../src/pogosim/version.h:
-../../src/pogosim/pogosim.h:
-../../src/pogosim/spogobot.h:
-../../src/pogosim/FifoBuffer.h:
